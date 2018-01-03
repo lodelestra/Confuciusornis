b599cd29b54299c16c231e6571836dcdd44e851d
lodelestra <loicdelestra@hotmail.fr>
2018-01-03 15:52:03 +0100

    remove avatar and theme warning

--- src/pages/configurator/PSUsDialog.jsx
+++ src/pages/configurator/PSUsDialog.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
-import Avatar from 'material-ui/Avatar';
-import List, { ListItem, ListItemAvatar, ListItemText } from 'material-ui/List';
+import List, { ListItem, ListItemText } from 'material-ui/List';
 import Dialog, { DialogTitle } from 'material-ui/Dialog';
 import Divider from 'material-ui/Divider';
-import PersonIcon from 'material-ui-icons/Person';
 import blue from 'material-ui/colors/blue';
 
 const styles = theme => ({
